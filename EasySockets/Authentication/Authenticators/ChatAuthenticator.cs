96e52780d5acc428e647b5e2a0404e84eb123889
Sil Gosker <silgosker@gmail.com>
2023-10-31 09:29:22 +0100

    Added authenticator example project

---
+++ EasySockets/Authentication/Authenticators/ChatAuthenticator.cs
@@ -0,0 +1,23 @@
+﻿using EasySockets.Authentication;
+using EasySockets.Services;
+
+public class ChatAuthenticator : IEasySocketAuthenticator
+{
+    private readonly IEasySocketService _easySocketService;
+
+    public ChatAuthenticator(IEasySocketService easySocketService)
+    {
+	    _easySocketService = easySocketService;
+    }
+
+    public EasySocketAuthenticationResult Authenticate(EasySocketAuthenticationResult currentAuthenticationResult,
+        HttpContext context)
+    {
+        if (!context.Request.Query.TryGetValue("slug", out var slug))
+        {
+            return false;
+        }
+
+        return new EasySocketAuthenticationResult(true, slug, _easySocketService.Count(slug).ToString());
+    }
+}
