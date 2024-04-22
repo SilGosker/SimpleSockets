835f3237bebe20c58441d0abd410bf095c146c8c
Sil Gosker <silgosker@gmail.com>
2024-04-22 09:29:58 +0200

    Added CustomEventTypes example project

---
+++ EasySockets/CustomEventTypes/Events/XmlEvent.cs
@@ -0,0 +1,9 @@
+﻿using EasySockets.Events;
+
+namespace CustomEventTypes.Events;
+
+public class XmlEvent : IEasySocketEvent
+{
+    public string Event { get; set; } = null!;
+    public string Message { get; set; } = null!;
+}
---
+++ EasySockets/CustomEventTypes/Sockets/XmlEventSocket.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml.Serialization;
+using CustomEventTypes.Events;
+using EasySockets.Events;
+
+namespace CustomEventTypes.Sockets;
+
+public class XmlEventSocket : EventSocket<XmlEvent>
+{
+    private static readonly XmlSerializer _serializer = new(typeof(XmlEvent));
+
+    public override XmlEvent? ExtractEvent(string message)
+    {
+        try
+        {
+            using var reader = new StringReader(message);
+            return (XmlEvent?)_serializer.Deserialize(reader);
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    public override string? BindEvent(string @event, string message)
+    {
+        var xmlEvent = new XmlEvent
+        {
+            Event = @event,
+            Message = message
+        };
+
+        using var writer = new StringWriter();
+        
+        _serializer.Serialize(writer, xmlEvent);
+        
+        return writer.ToString();
+    }
+}
