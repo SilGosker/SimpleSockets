820693c47dee4154df4ed515dc1e8664a1187190
Sil Gosker <silgosker@gmail.com>
2024-04-22 09:31:06 +0200

    improved readability on the ChatSocket example

--- EasySockets/EasySocketBasicChat/Sockets/ChatSocket.cs
+++ EasySockets/EasySocketBasicChat/Sockets/ChatSocket.cs
@@ -1,5 +1,4 @@
 ﻿using EasySockets;
-using EasySockets.Enums;
 
 namespace EasySocketBasicChat.Sockets;
 
@@ -8,15 +7,5 @@ public class ChatSocket : EasySocket
     public override Task OnMessage(string message)
     {
         return Broadcast(message);
-    }
-
-    public override Task OnConnect()
-    {
-        return Broadcast(BroadCastFilter.EqualRoomId, $"Welcome to '{ClientId}' for joining the chat!");
-    }
-
-    public override Task OnDisconnect()
-    {
-        return Broadcast($"User '{ClientId}' has left the chat!");
     }
 }
