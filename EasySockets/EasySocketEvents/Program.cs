90f26084a0dcef2ff4f2b287ad2661ae0942e79f
Sil Gosker <silgosker@gmail.com>
2024-04-22 09:31:38 +0200

    Added EasySocketEvents example project

---
+++ EasySockets/EasySocketEvents/Program.cs
@@ -0,0 +1,19 @@
+using EasySocketEvents.Sockets;
+using EasySockets.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEasySocketServices();
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseEasySockets()
+    .AddEasySocket<ChatSocket>("/chat");
+
+app.Run();
---
+++ EasySockets/EasySocketEvents/Sockets/ChatSocket.cs
@@ -0,0 +1,27 @@
+﻿using EasySockets.Attributes;
+using EasySockets.Enums;
+using EasySockets.Events;
+
+namespace EasySocketEvents.Sockets;
+
+public class ChatSocket : EventSocket
+{
+    [InvokeOn("Typing")]
+    [InvokeOn("typing")]
+    public Task SomeoneTyped()
+    {
+        return Broadcast(new EasySocketEvent("Typing", ClientId + " is typing..."));
+    }
+
+    [InvokeOn("Message")]
+    [InvokeOn("message")]
+    public Task MessageReceived(EasySocketEvent @event)
+    {
+        return Broadcast(@event);
+    }
+
+    public override Task OnConnect()
+    {
+        return Broadcast(BroadCastFilter.EqualRoomId, new EasySocketEvent("Connected", $"Welcome {ClientId}. You are currently in room '{RoomId}'"));
+    }
+}
