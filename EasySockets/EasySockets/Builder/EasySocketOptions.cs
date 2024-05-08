02119c20f76adad3695b4c53d9c2ad4fb6b2baae
Sil Gosker <silgosker@gmail.com>
2024-05-08 11:03:37 +0200

    Updated text in ObseleteAttribute

--- EasySockets/EasySockets/Builder/EasySocketOptions.cs
+++ EasySockets/EasySockets/Builder/EasySocketOptions.cs
@@ -18,7 +18,7 @@ public sealed class EasySocketOptions
     ///     The size of chunks when receiving or sending messages. <br />
     ///     Default is 100 bytes (100B).
     /// </summary>
-    [Obsolete($"Use {nameof(ReceiveBufferSize)} and {nameof(SendBufferSize)} instead")]
+    [Obsolete($"Use {nameof(ReceiveBufferSize)} and {nameof(SendBufferSize)} instead. This property will be removed in future versions.")]
     public int BufferSize
     {
         get => _bufferSize;
