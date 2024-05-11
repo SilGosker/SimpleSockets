1d44594addc1344b8d57b532d80703f7a6876921
Sil Gosker <silgosker@gmail.com>
2024-05-11 21:18:02 +0200

    Added unit tests

--- EasySockets/EasySocketsTests/Mock/MockEasySocketAuthenticator.cs
+++ EasySockets/EasySocketsTests/Mock/MockEasySocketAuthenticator.cs
@@ -8,6 +8,12 @@ public class MockEasySocketAuthenticator : IEasySocketAuthenticator
     internal static bool Authenticated = true;
     internal const string RoomId = "MockEasySocketAuthenticator_TestRoomId";
     internal const string ClientId = "MockEasySocketAuthenticator_TestClientId";
+    internal static int ConstructorCallCount { get; set; }
+
+    public MockEasySocketAuthenticator()
+    {
+        ConstructorCallCount++;
+    }
     public EasySocketAuthenticationResult Authenticate(EasySocketAuthenticationResult currentAuthenticationResult,
         HttpContext context)
     {
