0e52fe4d1a4ee5c657e2134a4db38e8c07f6b16a
Sil Gosker <silgosker@gmail.com>
2024-06-07 11:00:59 +0200

    Updated the EasySocketService to use ConcurrentDictionary instead of list

--- EasySockets/EasySockets/Services/EasySocketService.cs
+++ EasySockets/EasySockets/Services/EasySocketService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using EasySockets.Enums;
 using EasySockets.Events;
 
@@ -5,31 +6,32 @@ namespace EasySockets.Services;
 
 internal sealed class EasySocketService : IEasySocketService
 {
-    private readonly List<EasySocketRoom> _rooms = new();
+    private readonly ConcurrentDictionary<string, List<IEasySocket>> _rooms = new();
 
     public bool Any()
     {
-        return _rooms.Count > 0;
+        return !_rooms.IsEmpty;
     }
 
     public bool Any(string roomId)
     {
-        return _rooms.Any(e => e.Id == roomId);
+        return _rooms.ContainsKey(roomId);
     }
 
     public bool Any(string roomId, string clientId)
     {
-        return _rooms.Any(e => e.Id == roomId && e.Sockets.Any(o => o.ClientId == clientId && o.IsConnected()));
+        return _rooms.TryGetValue(roomId, out var easySockets) &&
+               easySockets.Any(o => o.ClientId == clientId && o.IsConnected());
     }
 
     public int Count()
     {
-        return _rooms.Sum(e => e.Sockets.Count);
+        return _rooms.Sum(e => e.Value.Count);
     }
 
     public int Count(string roomId)
     {
-        return _rooms.FirstOrDefault(e => e.Id == roomId)?.Sockets.Count ?? 0;
+        return _rooms.TryGetValue(roomId, out var easySockets) ? easySockets.Count : 0;
     }
 
     public Task ForceLeaveAsync(string roomId)
@@ -39,8 +41,12 @@ internal sealed class EasySocketService : IEasySocketService
 
     public Task ForceLeaveAsync(string roomId, CancellationToken cancellationToken)
     {
-        return Task.WhenAll(_rooms.FirstOrDefault(e => e.Id == roomId)?.Sockets
-            .Select(e => e.CloseAsync(cancellationToken)) ?? Enumerable.Empty<Task>());
+        if (_rooms.TryGetValue(roomId, out var easySockets))
+        {
+            return Task.WhenAll(easySockets.Select(e => e.CloseAsync(cancellationToken)));
+        }
+
+        return Task.CompletedTask;
     }
 
     public Task ForceLeaveAsync(string roomId, string clientId)
@@ -50,9 +56,13 @@ internal sealed class EasySocketService : IEasySocketService
 
     public Task ForceLeaveAsync(string roomId, string clientId, CancellationToken cancellationToken)
     {
-        return _rooms.FirstOrDefault(e => e.Id == roomId)?.Sockets
-                   .FirstOrDefault(e => e.IsConnected() && e.ClientId == clientId)?.CloseAsync(cancellationToken) ??
-               Task.CompletedTask;
+        if (_rooms.TryGetValue(roomId, out var easySockets))
+        {
+            return Task.WhenAll(easySockets.Where(e => e.ClientId == clientId)
+                .Select(e => e.CloseAsync(cancellationToken)));
+        }
+
+        return Task.CompletedTask;
     }
 
     public Task SendToRoomAsync(string roomId, string message)
@@ -62,8 +72,12 @@ internal sealed class EasySocketService : IEasySocketService
 
     public Task SendToRoomAsync(string roomId, string message, CancellationToken cancellationToken)
     {
-        return Task.WhenAll(_rooms.Where(e => e.Id == roomId)
-            .SelectMany(room => room.Sockets).Select(e => e.SendToClientAsync(message, cancellationToken)));
+        if (_rooms.TryGetValue(roomId, out var easySockets))
+        {
+            return Task.WhenAll(easySockets.Select(e => e.SendToClientAsync(message, cancellationToken)));
+        }
+
+        return Task.CompletedTask;
     }
 
     public Task SendToRoomAsync(string roomId, string @event, string message)
@@ -73,12 +87,14 @@ internal sealed class EasySocketService : IEasySocketService
 
     public Task SendToRoomAsync(string roomId, string @event, string message, CancellationToken cancellationToken)
     {
-        return Task.WhenAll(_rooms.SingleOrDefault(e => e.Id == roomId)?.Sockets
-                                .Where(e => e.IsConnected())
-                                .Select(e => e is IEventSocket o
-                                    ? o.SendToClientAsync(@event, message, cancellationToken)
-                                    : Task.CompletedTask)
-                            ?? Enumerable.Empty<Task>());
+        if (_rooms.TryGetValue(roomId, out var easySockets))
+        {
+            return Task.WhenAll(easySockets.Select(e => e is IEventSocket o
+                ? o.SendToClientAsync(@event, message, cancellationToken)
+                : Task.CompletedTask));
+        }
+
+        return Task.CompletedTask;
     }
 
     public Task SendToClientAsync(string roomId, string clientId, string message)
@@ -88,9 +104,13 @@ internal sealed class EasySocketService : IEasySocketService
 
     public Task SendToClientAsync(string roomId, string clientId, string message, CancellationToken cancellationToken)
     {
-        return _rooms.FirstOrDefault(e => e.Id == roomId)
-            ?.Sockets.FirstOrDefault(e => e.ClientId == clientId)
-            ?.SendToClientAsync(message, cancellationToken) ?? Task.CompletedTask;
+        if (_rooms.TryGetValue(roomId, out var easySockets))
+        {
+            return Task.WhenAll(easySockets.Where(e => e.ClientId == clientId)
+                .Select(e => e.SendToClientAsync(message, cancellationToken)));
+        }
+
+        return Task.CompletedTask;
     }
 
     public Task SendToClientAsync(string roomId, string clientId, string @event, string message)
@@ -101,17 +121,20 @@ internal sealed class EasySocketService : IEasySocketService
     public Task SendToClientAsync(string roomId, string clientId, string @event, string message,
         CancellationToken cancellationToken)
     {
-        return Task.WhenAll(_rooms.FirstOrDefault(e => e.Id == roomId)?.Sockets
-                                .Where(e => e.IsConnected() && e.ClientId == clientId)
-                                .Select(e => e is IEventSocket o
-                                    ? o.SendToClientAsync(@event, message, cancellationToken)
-                                    : Task.CompletedTask)
-                            ?? Enumerable.Empty<Task>());
+        if (_rooms.TryGetValue(roomId, out var easySockets))
+        {
+            return Task.WhenAll(easySockets.Where(e => e.ClientId == clientId)
+                           .Select(e => e is IEventSocket o
+                                              ? o.SendToClientAsync(@event, message, cancellationToken)
+                                                                 : Task.CompletedTask));
+        }
+
+        return Task.CompletedTask;
     }
 
     public IEnumerable<IGrouping<string, IEasySocket>> GetGroupings()
     {
-        return _rooms.SelectMany(e => e.Sockets).GroupBy(e => e.RoomId);
+        return _rooms.SelectMany(e => e.Value).GroupBy(e => e.RoomId);
     }
 
     internal async Task AddSocket(IEasySocket socket)
@@ -121,11 +144,10 @@ internal sealed class EasySocketService : IEasySocketService
         socket.Emit = BroadCast;
         socket.DisposeAtSocketHandler = RemoveSocket;
 
-        var room = _rooms.FirstOrDefault(e => e.Id == socket.RoomId);
-        if (room == null)
-            _rooms.Add(new EasySocketRoom(socket.RoomId, socket));
-        else
-            room.Sockets.Add(socket);
+        if (!_rooms.TryAdd(socket.RoomId, new List<IEasySocket>() { socket }))
+        {
+            _rooms[socket.RoomId].Add(socket);
+        }
 
         await socket.OnConnect().ConfigureAwait(false);
         await socket.ReceiveMessagesAsync().ConfigureAwait(false);
@@ -136,10 +158,12 @@ internal sealed class EasySocketService : IEasySocketService
         if (string.IsNullOrEmpty(message) || sender == null || broadCastFilter == BroadCastFilter.Everyone)
             return Task.CompletedTask;
 
-        var simpleSockets = _rooms.SelectMany(e => e.Sockets);
+        var simpleSockets = _rooms.SelectMany(e => e.Value);
+
         if (broadCastFilter.HasFlag(BroadCastFilter.EqualRoomId))
-            simpleSockets = _rooms.FirstOrDefault(room => room.Id == sender.RoomId)?.Sockets ??
-                            Enumerable.Empty<IEasySocket>();
+            simpleSockets = _rooms.TryGetValue(sender.RoomId, out var easySockets)
+                ? easySockets
+                : Enumerable.Empty<IEasySocket>();
 
         if (broadCastFilter.HasFlag(BroadCastFilter.EqualType))
             simpleSockets = simpleSockets.Where(e => e.GetType() == sender.GetType());
@@ -152,11 +176,10 @@ internal sealed class EasySocketService : IEasySocketService
 
     internal void RemoveSocket(IEasySocket caster)
     {
-        var room = _rooms.FirstOrDefault(e => e.Id == caster.RoomId);
-
-        if (room == null) return;
-
-        room.Sockets.RemoveAll(e => e.ClientId == caster.ClientId);
-        if (room.Sockets.Count < 1) _rooms.Remove(room);
+        if (_rooms.TryGetValue(caster.RoomId, out var easySockets))
+        {
+            easySockets.RemoveAll(e => e.ClientId == caster.ClientId);
+            if (easySockets.Count < 1) _rooms.TryRemove(caster.RoomId, out _);
+        }
     }
 }
